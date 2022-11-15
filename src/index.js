1c243cc6e06666c463555ff4670298469d74c02b
Suzan <suzanbulbl@gmail.com>
2022-11-15 14:13:04 +0300

    request methods add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log("Test");
+import { Request } from "./request";
+
+// Element secme
+const form = document.getElementById("employee-form");
+const nameInput = document.getElementById("name");
+const departmentInput = document.getElementById("department");
+const salaryInput = document.getElementById("salary");
+const employeesList = document.getElementById("employees");
+const updateEmployeeButton = document.getElementById("update");
+
+const request = new Request("http://localhost:3000/employees");
+
+/*
+request.get()
+    .then(employees => {
+        console.log(employees)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+request.post({name: "Mertcan", department: "Product Manager", salary: 8000})
+    .then(employees => {
+        console.log(employees)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+   
+request.put(3, {name: "Mertcan", department: "Product Manager", salary: 8000})  
+    .then(employees => {
+        console.log(employees)
+    })
+    .catch(err => {
+        console.log(err)
+    })  
+     */
+    request.delete(3).then(message => {
+        console.log(message)
+    }).catch(err => {
+        console.log(err)
+    })
+
+
